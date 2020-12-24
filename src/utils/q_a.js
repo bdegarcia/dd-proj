e8a80951ddb8358ef37c9e0cb6cdde7649becf22
ben <degarcia.ben@gmail.com>
2020-12-24 14:23:27 -0600

    added Q&A file

---
+++ src/utils/q_a.js
@@ -0,0 +1,29 @@
+export default Questions
+
+const Questions = [
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+    {
+        quest: 'WHAT IS AUTO REMARKETING SERVICE?',
+        ans: 'We are a group of automotive professionals who want to help you get rid of aged/unwanted inventory, while still maximizing profits for you.'
+    },
+
+]
